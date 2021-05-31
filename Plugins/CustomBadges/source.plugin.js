b30f1e1f87600a7c2846adb53a49a8fade4702dc
Xtract <41217280+AnyMinorDeerPanda@users.noreply.github.com>
2021-05-31 12:48:13 -0600

    Update source.plugin.js

--- Plugins/CustomBadges/source.plugin.js
+++ Plugins/CustomBadges/source.plugin.js
@@ -1,1 +1,8 @@
-
+/**
+ * @name CustomBadges
+ * @version 0.0.1
+ * @description Allows for Custom Badges on Discord
+ * @author Xtract
+ * @website https://badges.websitez2999.com
+ * @updateUrl https://raw.githubusercontent.com/AnyMinorDeerPanda/BetterDiscord/main/Plugins/CustomBadges/source.plugin.js
+ */
