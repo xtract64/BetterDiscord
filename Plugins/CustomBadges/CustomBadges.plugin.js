2a1fdc408cf4b8ac681e5cef4665f69c034566d6
Xtract <41217280+AnyMinorDeerPanda@users.noreply.github.com>
2021-05-31 13:01:28 -0600

    Update CustomBadges.plugin.js

--- Plugins/CustomBadges/CustomBadges.plugin.js
+++ Plugins/CustomBadges/CustomBadges.plugin.js
@@ -6,5 +6,23 @@
  * @authorID 532709351887077385
  * @invite 37jw4DYbBb
  * @website https://badges.websitez2999.com
- * @updateUrl https://raw.githubusercontent.com/AnyMinorDeerPanda/BetterDiscord/main/Plugins/CustomBadges/source.plugin.js
+ * @source https://github.com/AnyMinorDeerPanda/BetterDiscord/tree/main/Plugins/CustomBadges
+ * @updateUrl https://raw.githubusercontent.com/AnyMinorDeerPanda/BetterDiscord/main/Plugins/CustomBadges/CustomBadges.plugin.js
  */
+
+module.exports = (_ => {
+	const config = {
+		"info": {
+			"name": "CustomBadges",
+			"author": "Xtract",
+			"version": "0.0.1",
+			"description": "Allows for Custom Badges on Discord"
+		},
+		"changeLog": {
+			"Whats this?": {
+				"It's Alive!": "Plugin Created."
+			},
+			
+		}
+	};
+	
