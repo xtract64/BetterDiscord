02e52d62873b763ea07701b052d7f5a091c2e257
Xtract <41217280+AnyMinorDeerPanda@users.noreply.github.com>
2021-05-31 13:11:46 -0600

    Update CustomBadges.plugin.js

--- Plugins/CustomBadges/CustomBadges.plugin.js
+++ Plugins/CustomBadges/CustomBadges.plugin.js
@@ -1,28 +1,19 @@
 /**
  * @name CustomBadges
- * @version 0.0.1
- * @description Allows for Custom Badges on Discord
  * @author Xtract
- * @authorID 532709351887077385
+ * @description Allows for Custom Badges on Discord
+ * @version 0.0.1
  * @invite 37jw4DYbBb
- * @website https://badges.websitez2999.com
+ * @authorId 532709351887077385
  * @source https://github.com/AnyMinorDeerPanda/BetterDiscord/tree/main/Plugins/CustomBadges
  * @updateUrl https://raw.githubusercontent.com/AnyMinorDeerPanda/BetterDiscord/main/Plugins/CustomBadges/CustomBadges.plugin.js
  */
 
-module.exports = (_ => {
-	const config = {
-		"info": {
-			"name": "CustomBadges",
-			"author": "Xtract",
-			"version": "0.0.1",
-			"description": "Allows for Custom Badges on Discord"
-		},
-		"changeLog": {
-			"Whats this?": {
-				"It's Alive!": "Plugin Created."
-			}
-			
-		}
-	};
-	
+module.exports = class ExamplePlugin {
+    load() {} // Optional function. Called when the plugin is loaded in to memory
+
+    start() {} // Required function. Called when the plugin is activated (including after reloads)
+    stop() {} // Required function. Called when the plugin is deactivated
+
+    observer(changes) {} // Optional function. Observer for the `document`. Better documentation than I can provide is found here: <https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver>
+}
