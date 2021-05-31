ba039e819148c82f1364056f30b797827d355d21
Xtract <41217280+AnyMinorDeerPanda@users.noreply.github.com>
2021-05-31 12:56:13 -0600

    Update and rename source.plugin.js to CustomBadges.plugin.js

--- Plugins/CustomBadges/CustomBadges.plugin.js
+++ Plugins/CustomBadges/CustomBadges.plugin.js
@@ -3,6 +3,8 @@
  * @version 0.0.1
  * @description Allows for Custom Badges on Discord
  * @author Xtract
+ * @authorID 532709351887077385
+ * @invite 37jw4DYbBb
  * @website https://badges.websitez2999.com
  * @updateUrl https://raw.githubusercontent.com/AnyMinorDeerPanda/BetterDiscord/main/Plugins/CustomBadges/source.plugin.js
  */
